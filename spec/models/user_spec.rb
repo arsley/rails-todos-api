28fc2515099dc6f6f949d20f13ff7a6d4a67b390
assly <blue20will@gmail.com>
2018-08-01 10:01:01 +0900

    add: Create User model for auth

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # assosiation test
+  # ensure User model has a 1:m relationship with the Todo model
+  it { should have_many(:todos) }
+  # validation tests
+  # ensure name, email, password_digest are present before save
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+end
